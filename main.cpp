5072425e65b5621671f9361561bf941a997be5f1
markclifton <30988894+markclifton@users.noreply.github.com>
2019-03-25 21:43:26 -0400

    Example ECS code

--- main.cpp
+++ main.cpp
@@ -36,11 +36,51 @@ public:
 	//se::ecs::TestSystem testSystem;
 };*/
 
+struct testComp
+{
+	int x = 0;
+};
+
+class TestSystem : public se::EntitySystem
+{
+public:
+	TestSystem() = default; 
+	~TestSystem() = default;
+
+	void tick(se::World* world, float deltaTime) override
+	{
+		world->each<testComp>([&](se::Entity* ent, se::ComponentHandle<testComp> component) {
+			component->x += 1;
+		});
+	}
+};
+
 int main()
 {
 #ifdef WIN32
-	FreeConsole();
+	//FreeConsole();
 #endif
+
+	//Create World
+	auto world = se::World::createWorld();
+
+	//Create System
+	auto sys = new TestSystem();
+	world->registerSystem(sys);
+	
+	//Create Object
+	auto obj = world->create();
+	obj->assign<testComp>();
+
+	//Update Systems
+	for (int i = 0; i < 100; i++)
+	{
+		world->tick(1.f);
+	}
+
+	//Print Results
+	auto comp = obj->get<testComp>();
+	std::cout << comp.get().x << "\n";
 
 //	TestGame testGame;
 	se::managers::WindowManager wm{ "Hello, World!", 640, 480 };
